f21908d718b25ba434499e022ee319766aeebcb7
Uzair Khan <uzair@Uzairs-MacBook-Pro.local>
2022-09-25 17:23:41 +0300

    Removed unwanted theme files

--- am-content/Themes/nanabari/public/js/script.js
+++
@@ -1,9 +0,0 @@
-$('.header-box-top-area').on('click','li',function(e){
-    e.preventDefault();
-    $(this).addClass('active').siblings().removeClass('active');
-});
-
-function status_change(id)
-{
-    $('#property_status').val(id);
-}
