0d960be322955db272197026a673028a4d4b728b
usman-eng <70435503+usman-eng@users.noreply.github.com>
2022-11-20 15:05:44 +0300

    dropdown of icon js fix

--- public/assets/newjs/main.js
+++ public/assets/newjs/main.js
@@ -217,7 +217,7 @@ $(document).ready(function (event) {
     $('.property-type-drop').click(function (e) {
         $('.overlay').css('opacity', 0.2);
         $('.overlay').css('display', 'block');
-        event.stopPropagation();
+        // event.stopPropagation();
     });
 
     $('.overlay').click(function (event) {
@@ -235,7 +235,7 @@ $(document).ready(function (event) {
     $('.room-type-drop').click(function (e) {
         $('.overlay').css('opacity', 0.2);
         $('.overlay').css('display', 'block');
-        event.stopPropagation();
+        // event.stopPropagation();
     });
 
     $('.overlay').click(function (event) {
