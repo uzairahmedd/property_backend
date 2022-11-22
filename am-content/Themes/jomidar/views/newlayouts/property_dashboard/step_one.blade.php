d2f81e1138b7cd3490159997f59d2fc854e084f9
usman-eng <70435503+usman-eng@users.noreply.github.com>
2022-11-23 02:50:39 +0300

    create property page

--- am-content/Themes/jomidar/views/newlayouts/property_dashboard/step_one.blade.php
+++ am-content/Themes/jomidar/views/newlayouts/property_dashboard/step_one.blade.php
@@ -1,6 +1,9 @@
 @extends('theme::newlayouts.app')
 @section('content')
     <link rel="stylesheet" href="{{theme_asset('assets/newcss/property_step.css')}}">
+    @php
+$info = json_decode(Auth::User()->usermeta->content ?? '');
+@endphp
     <div class="add-property row-style">
         <div class="head text-center">
             <h1 class="font-bold theme-text-white pt-5">أدرج عقارك معنا</h1>
@@ -12,17 +15,17 @@
                     <span class="font-16 theme-text-sky">رقم الجوال</span>
                     <div class="d-flex align-items-center">
                         <img src="assets/images/tick-verified.png" alt="">
-                        <h3 class="font-24 font-medium theme-text-blue mb-0 ms-2">05546106060</h3>
+                        <h3 class="font-24 font-medium theme-text-blue mb-0 ms-2">{{ $info->phone ?? 'N/A' }}</h3>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 d-flex flex-column align-items-end">
                     <span class="font-16 theme-text-sky">مسجل منذ</span>
-                    <h3 class="font-24 font-medium theme-text-blue mb-0 ms-2">12-10-2022</h3>
+                    <h3 class="font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->created_at->format('m/d/Y') }}</h3>
                 </div>
                 <div class="col-lg-4 col-md-4 d-flex align-items-center flex-column-reverse flex-lg-row">
                     <div class="col d-flex flex-column align-items-end text-sm-right welcome justify-content-end">
                         <span class="font-16 theme-text-sky">أهلا بك,</span>
-                        <h3 class="font-24 font-medium theme-text-blue align-items-end mb-0 ms-2">خالد بن عبدالعزيز أل عثمان</h3>
+                        <h3 class="font-24 font-medium theme-text-blue align-items-end mb-0 ms-2">{{ Auth::User()->name }}</h3>
                     </div>
                     <div class="dp-elipse ms-4 d-flex align-items-center justify-content-center">
                         <img src="assets/images/avatar.png" alt="">
