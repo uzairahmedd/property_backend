edb3eaa0b56433d7f135f79d58c3b437ce8beb3a
usman-eng <70435503+usman-eng@users.noreply.github.com>
2022-12-04 03:38:50 +0300

    update phone page

--- am-content/Themes/jomidar/views/newlayouts/property_dashboard/finish.blade.php
+++ am-content/Themes/jomidar/views/newlayouts/property_dashboard/finish.blade.php
@@ -9,7 +9,7 @@
     @include('theme::newlayouts.partials.user_header')
     <!-- Property Description Section Starts Here -->
     <div class="container">
-        <form id="modify_phone">
+        <form id="modify_phones">
             @csrf
             <input type="hidden" name="user_id" value="{{encrypt(Auth::User()->id)}}">
             <div class="description-card card finished align-items-center">
@@ -28,7 +28,7 @@
                         <p class="font-medium theme-text-seondary-black">سيتم ارسال الطلبات على هذا الإعلان إلى</p>
                         <input type="phone" name="phone" value="{{ Auth::User()->phone ?? '' }}" placeholder="رقم الجوال" class="form-control theme-border">
                         <div class="d-flex">
-                            <button id="update_phone" type="submit" class="btn modify-btn font-medium font-14 theme-text-white b-r-8">تعديل <i class=""></i> </button>
+                            <button id="update_phones" type="submit" class="btn modify-btn font-medium font-14 theme-text-white b-r-8">تعديل <i class=""></i> </button>
                         </div>
                         <span id="phone_errors"></span>
                     </div>
