b97bdad5ce81f59a6f31ec709be93faf6c876b89
usman-eng <70435503+usman-eng@users.noreply.github.com>
2023-01-31 00:33:43 +0300

    map language and resouce condition

--- am-content/Themes/jomidar/views/newlayouts/pages/property_detail.blade.php
+++ am-content/Themes/jomidar/views/newlayouts/pages/property_detail.blade.php
@@ -523,6 +523,11 @@
     var coordinates = $('#map_coordinates').val();
     var array = coordinates.split(',');
     mapboxgl.accessToken = 'pk.eyJ1IjoicmFrYW5vbmxpbmUiLCJhIjoiY2xjeGpsMmdxMG05ajN2cXJocm5mazV3diJ9.puFe2Kj4KfE5v9Ky20ohYg';
+    mapboxgl.setRTLTextPlugin(
+        'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+        null,
+        true // Lazy load the plugin
+    );
     const map = new mapboxgl.Map({
         container: 'map', // Container ID
         style: 'mapbox://styles/mapbox/streets-v12', // Map style to use
