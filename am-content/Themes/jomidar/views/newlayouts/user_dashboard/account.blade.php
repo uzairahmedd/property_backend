1a024b72f306c816c0149ec4efdaa024693ce2d5
usman-eng <70435503+usman-eng@users.noreply.github.com>
2023-02-21 17:13:12 +0300

    add image upload from account

--- am-content/Themes/jomidar/views/newlayouts/user_dashboard/account.blade.php
+++ am-content/Themes/jomidar/views/newlayouts/user_dashboard/account.blade.php
@@ -1,23 +1,40 @@
 @extends('theme::newlayouts.app')
 @section('content')
-    <link rel="stylesheet" href="{{theme_asset('assets/newcss/second-page.css')}}">
-    <link rel="stylesheet" href="{{theme_asset('assets/newcss/profile.css')}}">
-    <div class="profile d-flex justify-content-end">
-            <div class="col-9 mx-auto">
-                <div class="main-content">
-                    <div class="mb-4_5 d-flex flex-column-reverse flex-lg-row align-items-end  card personal-card justify-content-between align-items-lg-center">
-                        <div class="d-flex flex-column align-items-end">
-                            <span class="font-16 theme-text-sky">{{__('labels.mobile_number')}}</span>
-                            <div class="d-flex align-items-center profile-phone-nu">
-                                <img src="http://127.0.0.1:8000/assets/images/tick-verified.png" alt="">
-                                <h3 class="font-lg-18 font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->phone ?? 'N/A' }}</h3>
+<link rel="stylesheet" href="{{theme_asset('assets/newcss/second-page.css')}}">
+<link rel="stylesheet" href="{{theme_asset('assets/newcss/profile.css')}}">
+<div class="profile d-flex justify-content-end">
+    <div class="col-9 mx-auto">
+        <div class="main-content">
+            <div class="mb-4_5 d-flex flex-column-reverse flex-lg-row align-items-end  card personal-card justify-content-between align-items-lg-center">
+                <div class="d-flex flex-column align-items-end">
+                    <span class="font-16 theme-text-sky">{{__('labels.mobile_number')}}</span>
+                    <div class="d-flex align-items-center profile-phone-nu">
+                        <img src="http://127.0.0.1:8000/assets/images/tick-verified.png" alt="">
+                        <h3 class="font-lg-18 font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->phone ?? 'N/A' }}</h3>
+                    </div>
+                </div>
+                <div class="d-flex flex-column align-items-end mb-3 mb-lg-0">
+                    <span class="font-16 theme-text-sky">{{__('labels.registered_since')}}</span>
+                    <h3 class="font-lg-18 font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->created_at->format('m/d/Y') }}</h3>
+                </div>
+                <form class="form-signin text-center" id="user_save_profile_form" method="post" action="{{ route('agent.profile.img') }}" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" id="image_id" name="id">
+                    <div class="d-flex align-items-center mb-3 mb-lg-0">
+                        <div class="col d-flex flex-column align-items-end">
+                            <span class="font-16 theme-text-sky">{{__('labels.welcome')}}</span>
+                            <h3 class="font-lg-18 font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->name }}</h3>
+                        </div>
+                        <div class="dp-elipse photo-row d-flex align-items-center justify-content-center">
+                            <img class="photo-img image_user" id="image_user" src="{{asset('assets/images/avatar.png')}}" alt="" class="img-fluid">
+                            <div class="file-container">
+                                <input type="file" id="profile_pic" name="image" />
+                                <img src="{{asset('assets/images/dp-camera.png')}}" alt="">
                             </div>
                         </div>
-                        <div class="d-flex flex-column align-items-end mb-3 mb-lg-0">
-                            <span class="font-16 theme-text-sky">{{__('labels.registered_since')}}</span>
-                            <h3 class="font-lg-18 font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->created_at->format('m/d/Y') }}</h3>
-                        </div>
-                        <div class="d-flex align-items-center mb-3 mb-lg-0">
+                    </div>
+                </form>
+                <!-- <div class="d-flex align-items-center mb-3 mb-lg-0">
                             <div class="col d-flex flex-column align-items-end">
                                 <span class="font-16 theme-text-sky">{{__('labels.welcome')}}</span>
                                 <h3 class="font-lg-18 font-24 font-medium theme-text-blue mb-0 ms-2">{{ Auth::User()->name }}</h3>
@@ -29,41 +46,40 @@
                                     <img src="http://127.0.0.1:8000/assets/images/dp-camera.png" alt="">
                                 </div>
                             </div>
+                        </div> -->
+            </div>
+            <div class="account-setting">
+                <form action="" class="row d-flex flex-wrap justify-content-end">
+                    <div class="col-lg-4 col-md-8 col-sm-12 accout-id">
+                        <div class="d-flex flex-column align-items-end">
+                            <label for="" class="font-16 theme-text-seondary-black mb-2">{{__('labels.email_optional')}}</label>
+                            <input type="email" value="{{ Auth::User()->email }}" name="email" placeholder="{{__('labels.email')}}" class="form-control">
                         </div>
                     </div>
-                    <div class="account-setting">
-                        <form action="" class="row d-flex flex-wrap justify-content-end">
-                            <div class="col-lg-4 col-md-8 col-sm-12 accout-id">
-                                <div class="d-flex flex-column align-items-end">
-                                    <label for="" class="font-16 theme-text-seondary-black mb-2">{{__('labels.email_optional')}}</label>
-                                    <input type="email" value="{{ Auth::User()->email }}" name="email" placeholder="{{__('labels.email')}}" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-md-8 col-sm-12">
-                                <div class="d-flex flex-column align-items-end">
-                                    <label for="" class="font-16 theme-text-seondary-black mb-2">{{__('labels.id')}}</label>
-                                    <input type="text" name="" id="" placeholder="123456788" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-12 d-flex justify-content-end mt-5 mb-4_5">
-                                <!-- <button class="btn acct-btn b-r-8  theme-bg-sky border-0 theme-text-white font-bold font-16">{{__('labels.save')}}</button> -->
-                            </div>
-                        </form>
-                        <hr class="mb-4_5">
-                        <div class="d-flex">
-                            <button class="btn btn-outline-danger">
-                                <a data-id="{{Auth::User()->id}}"  onclick="account_del(this)">{{__('labels.delete_account')}}</a>
-                            </button>
+                    <div class="col-lg-4 col-md-8 col-sm-12">
+                        <div class="d-flex flex-column align-items-end">
+                            <label for="" class="font-16 theme-text-seondary-black mb-2">{{__('labels.id')}}</label>
+                            <input type="text" name="" id="" placeholder="123456788" class="form-control">
                         </div>
                     </div>
+                    <div class="col-12 d-flex justify-content-end mt-5 mb-4_5">
+                        <!-- <button class="btn acct-btn b-r-8  theme-bg-sky border-0 theme-text-white font-bold font-16">{{__('labels.save')}}</button> -->
+                    </div>
+                </form>
+                <hr class="mb-4_5">
+                <div class="d-flex">
+                    <button class="btn btn-outline-danger">
+                        <a data-id="{{Auth::User()->id}}" onclick="account_del(this)">{{__('labels.delete_account')}}</a>
+                    </button>
                 </div>
             </div>
-        @include('theme::newlayouts.partials.sidebar')
+        </div>
     </div>
+    @include('theme::newlayouts.partials.sidebar')
+</div>
 @endsection
 
 @section('account_js')
-    <script src="{{theme_asset('assets/newjs/user_account.js')}}"></script>
-    <script src="{{theme_asset('assets/newjs/property_step.js')}}"></script>
+<script src="{{theme_asset('assets/newjs/user_account.js')}}"></script>
+<script src="{{theme_asset('assets/newjs/property_step.js')}}"></script>
 @endsection
-
